7884d0188a3575887daf176068b46333dcf0a5a0
stone1100 <stonejh1100@gmail.com>
2022-06-08 12:33:28 +0800

    add docsearch plugin

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,5 +1,5 @@
 import { defineUserConfig } from 'vuepress'
-import { searchPlugin } from '@vuepress/plugin-search'
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
 import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
 import { path } from '@vuepress/utils'
 import { navbar, sidebar } from './configs'
@@ -58,13 +58,71 @@ export default defineUserConfig({
         tip: '提示',
         warning: '注意',
         danger: '警告',
+         // 404 page
+        notFound: [
+          '这里什么都没有',
+          '我们怎么到这来了？',
+          '这是一个 404 页面',
+          '看起来我们进入了错误的链接',
+        ],
+        backToHome: '返回首页',
         toggleDarkMode: '切换夜间模式',
         tocNavText: '本页导航',
       },
     },
   }),
   plugins: [
-    searchPlugin({
+    docsearchPlugin({
+      appId: 'Z2Q2CCBG6Q',
+      apiKey: 'a54e7b4183d11c7f6967d67dd9801ade',
+      indexName: 'lindb',
+      // searchParameters: {
+      //   facetFilters: ['tags:v2'],
+      // },
+      // debug: true,
+      locales: {
+        '/zh/': {
+          placeholder: '搜索文档',
+          translations: {
+            button: {
+              buttonText: '搜索文档',
+              buttonAriaLabel: '搜索文档',
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: '清除查询条件',
+                resetButtonAriaLabel: '清除查询条件',
+                cancelButtonText: '取消',
+                cancelButtonAriaLabel: '取消',
+              },
+              startScreen: {
+                recentSearchesTitle: '搜索历史',
+                noRecentSearchesText: '没有搜索历史',
+                saveRecentSearchButtonTitle: '保存至搜索历史',
+                removeRecentSearchButtonTitle: '从搜索历史中移除',
+                favoriteSearchesTitle: '收藏',
+                removeFavoriteSearchButtonTitle: '从收藏中移除',
+              },
+              errorScreen: {
+                titleText: '无法获取结果',
+                helpText: '你可能需要检查你的网络连接',
+              },
+              footer: {
+                selectText: '选择',
+                navigateText: '切换',
+                closeText: '关闭',
+                searchByText: '搜索提供者',
+              },
+              noResultsScreen: {
+                noResultsText: '无法找到相关结果',
+                suggestedQueryText: '你可以尝试查询',
+                reportMissingResultsText: '你认为该查询应该有结果？',
+                reportMissingResultsLinkText: '点击反馈',
+              },
+            },
+          },
+        },
+      },
     }),
     registerComponentsPlugin({
       componentsDir: path.resolve(__dirname, './components'),
